bf23bf9bb8507c4ea1fc9c2e1d6351f7dfefa328
simonkim1 <sevencuter@gmail.com>
2022-07-07 15:47:00 +0900

    React_배열 렌더링하기(내용 추가)

--- example/src/components/UserList.js
+++ example/src/components/UserList.js
@@ -47,11 +47,11 @@ function User({ user }) {
 }
 
 function UserList() {
-    const USERS = [
+    const USERS1 = [
         {
             id: 1,
             userName: "Kim",
-            email: "kim@gmail.com"
+            email: "Kim@gmail.com"
         },
         {
             id: 2,
@@ -62,6 +62,20 @@ function UserList() {
             id: 3,
             userName: "Choi",
             email: "Choi@daum.net"
+        }
+    ];
+    const USERS2 = [
+        {
+            userName: "kim",
+            email: "kim@gmail.com"
+        },
+        {
+            userName: "lee",
+            email: "lee@naver.com"
+        },
+        {
+            userName: "choi",
+            email: "choi@daum.net"
         }
     ];
     return (
@@ -75,9 +89,24 @@ function UserList() {
 
         // ↓↓ 동적인 배열인 경우 map을 이용해 loop로 처리
         <div>
-            {USERS.map(user => (
-                <User user={user} key={user.id}/>
+            {USERS1.map(user => (
+                <User user={user} key={user.id}
+                    // 배열 안의 원소가 가지고 있는 고휴한 값이 있는 경우
+                />
             ))}
+            <br />
+            <br />
+            {USERS2.map((user, index) => (
+                <User user={user} key={index}
+                    // 배열 안의 원소가 가지고 있는 고휴한 값이 없는 경우에는 index로 사용가능
+                />
+            ))}
+
+            {/*
+                배열을 랜더링 할 때 key 설정을 하지 않는다면 기본적으로 배열의 index 값을 key로 사용 하게 된다.
+                하지만, 각 고유 원소에 key가 있어야만 배열이 업데이트 될 때 효율적으로 렌더링이 될 수 있기 때문에
+                key를 설정 하지 않고 사용 하게 되면 경고메세지가 뜨게 된다.
+            */}
         </div>
     );
 }
